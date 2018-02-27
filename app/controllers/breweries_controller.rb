34b6dc9bca76f9397d1d20898de1ca94b42cb3d8
Romain niam0r <niamor@mm.st>
2018-02-27 16:09:05 +0000

    generated breweries_controller and created its index.html.erb

---
+++ app/controllers/breweries_controller.rb
@@ -0,0 +1,13 @@
+class BreweriesController < ApplicationController
+  def index
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
+  end
+end
