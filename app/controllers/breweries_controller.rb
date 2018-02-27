6f16ddeff7ed0e22bbcf9006b4ba916da1e628c1
Romain niam0r <niamor@mm.st>
2018-02-27 18:04:44 +0000

    implemented new and show pages

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,4 +1,6 @@
 class BreweriesController < ApplicationController
+  before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+
   def index
     @breweries = Brewery.where.not(latitude: nil, longitude: nil)
 
@@ -9,5 +11,47 @@ class BreweriesController < ApplicationController
         # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
       }
     end
+  end
+
+  def new
+    @brewery = Brewery.new
+  end
+
+  def create
+    @brewery = Brewery.new(brewery_params)
+    if @brewery.save
+      redirect_to brewery_path(@brewery)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @brewery.update(brewery_params)
+      redirect_to breweries_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @brewery.destroy
+    redirect_to breweries_path
+  end
+
+  private
+
+  def set_brewery
+    @brewery = Brewery.find(params[:id])
+  end
+
+  def brewery_params
+    params.require(:brewery).permit(:name, :address)
   end
 end
