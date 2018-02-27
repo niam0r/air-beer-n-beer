8f99959c79dfaf26d997fd68a053c4ab9aefb153
Romain niam0r <niamor@mm.st>
2018-02-27 12:50:38 +0000

    rails g model Brewery name address

---
+++ db/migrate/20180227124932_create_breweries.rb
@@ -0,0 +1,10 @@
+class CreateBreweries < ActiveRecord::Migration[5.1]
+  def change
+    create_table :breweries do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227115046) do
+ActiveRecord::Schema.define(version: 20180227124932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "breweries", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
